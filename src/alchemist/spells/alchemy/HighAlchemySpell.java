7b904e18697071437d849d2b66d4a7f01ef5f198
Patrick Sutton <psutton3756@yahoo.com>
2015-02-03 02:03:27 -0500

    Minor refactoring changes.

--- src/alchemist/spells/alchemy/HighAlchemySpell.java
+++ src/alchemist/spells/alchemy/HighAlchemySpell.java
@@ -10,24 +10,13 @@ import helpers.objects.RuneForSpell;
 public class HighAlchemySpell extends MagicSpell {
 
 	public HighAlchemySpell() {
+		RuneForSpell natureRune = new RuneForSpell("Nature Rune", AlchemistGlobal.runeNature, 1, false, false);
+		RuneForSpell fireRune = new RuneForSpell("Fire Rune", AlchemistGlobal.runeFire, 5, true, true);
+		RuneForSpell[] requiredRunes = { natureRune };
+		
+		this.setRequiredRunes(requiredRunes);
 		this.setSpell(Magic.Spell.HIGH_LEVEL_ALCHEMY);
 		this.setSpellName("High Alchemy");
 		this.setTimeToCast(3000);
-		
-		int[] staves = { AlchemistGlobal.staffFire,
-				AlchemistGlobal.staffLava,
-				AlchemistGlobal.staffSteam 
-		};
-		this.setStaves(staves);
-		
-		RuneForSpell natureRune = new RuneForSpell("Nature Rune", AlchemistGlobal.runeNature, 1, false, false);
-		RuneForSpell[] requiredRunes = { natureRune };
-		this.setRequiredRunes(requiredRunes);
-		
-		RuneForSpell fireRune = new RuneForSpell("Fire Rune", AlchemistGlobal.runeFire, 5, true, true);
-		RuneForSpell steamRune = new RuneForSpell("Steam Rune", AlchemistGlobal.runeSteam, 5, false, true);
-		RuneForSpell lavaRune = new RuneForSpell("Lava Rune", AlchemistGlobal.runeLava, 5, false, true);
-		RuneForSpell[] substituteRunes = { fireRune, steamRune, lavaRune };
-		this.setSubstituteRunes(substituteRunes);
 	}
 }
--- src/helpers/objects/MagicSpell.java
+++ src/helpers/objects/MagicSpell.java
@@ -4,19 +4,25 @@ import com.epicbot.api.rs3.methods.tab.Magic;
 
 public class MagicSpell {
 	private Magic.Spell spell;
+	private RuneForSpell[] requiredRunes;
 	private String spellName;
 	private int timeToCast;
-	private int[] staves;
-	private RuneForSpell[] requiredRunes;
-	private RuneForSpell[] substituteRunes;
 	
-
+		
 	public Magic.Spell getSpell() {
 		return spell;
 	}
 
 	public void setSpell(Magic.Spell spell) {
 		this.spell = spell;
+	}
+
+	public RuneForSpell[] getRequiredRunes() {
+		return requiredRunes;
+	}
+
+	public void setRequiredRunes(RuneForSpell[] requiredRunes) {
+		this.requiredRunes = requiredRunes;
 	}
 
 	public String getSpellName() {
@@ -30,33 +36,9 @@ public class MagicSpell {
 	public int getTimeToCast() {
 		return timeToCast;
 	}
-
+	
 	public void setTimeToCast(int timeToCast) {
 		this.timeToCast = timeToCast;
-	}
-
-	public int[] getStaves() {
-		return staves;
-	}
-
-	public void setStaves(int[] staves) {
-		this.staves = staves;
-	}
-
-	public RuneForSpell[] getRequiredRunes() {
-		return requiredRunes;
-	}
-
-	public void setRequiredRunes(RuneForSpell[] requiredRunes) {
-		this.requiredRunes = requiredRunes;
-	}
-
-	public RuneForSpell[] getSubstituteRunes() {
-		return substituteRunes;
-	}
-
-	public void setSubstituteRunes(RuneForSpell[] substituteRunes) {
-		this.substituteRunes = substituteRunes;
 	}
 
 	@Override
