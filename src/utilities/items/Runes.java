b0e95c7383fa275fad5c479bc64c51b8de84f226
Patrick Sutton <psutton3756@yahoo.com>
2015-02-03 21:13:07 -0500

    Added list of rune ids.

---
+++ src/utilities/items/Runes.java
@@ -0,0 +1,24 @@
+package utilities.items;
+
+public class Runes {
+	public final static int Air = 556;
+	public final static int Armadyl = 21733;
+	public final static int Astral = 9075;
+	public final static int Blood = 565;
+	public final static int Body = 559;
+	public final static int Chaos = 562;
+	public final static int Cosmic = 564;
+	public final static int Death = 560;
+	public final static int Dust = 4696;
+	public final static int Earth = 556;
+	public final static int Fire = 554;
+	public final static int Lava = 557;
+	public final static int Law =	4699;
+	public final static int Mind =558;
+	public final static int Mist = 4695;
+	public final static int Mud = 4698;
+	public final static int Nature = 561;
+	public final static int Smoke = 4697;
+	public final static int Soul = 566;
+	public final static int Steam = 4694;
+}
