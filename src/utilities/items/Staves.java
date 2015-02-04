629101318e1ac83d4ac511b3762e352a0e1ecdcb
Patrick Sutton <psutton3756@yahoo.com>
2015-02-03 21:38:35 -0500

    Added list of staves.

---
+++ src/utilities/items/Staves.java
@@ -0,0 +1,23 @@
+package utilities.items;
+
+public class Staves {
+	public final static int AirStaff = 1381;
+	public final static int AirBattle = 1398;
+	public final static int AirMystic = 1405;
+	public final static int ArmadylBattle = 21777;
+	public final static int EarthStaff = 1385;
+	public final static int EarthBattle = 1399;
+	public final static int EarthMystic = 1407;
+	public final static int FireStaff = 1387;
+	public final static int FireBattle = 1393;
+	public final static int FireMystic = 1401;
+	public final static int LavaBattle = 3053;
+	public final static int LavaMystic = 3054;
+	public final static int MudBattle = 6562;
+	public final static int MudMystic = 6563;
+	public final static int SteamBattle = 11736;
+	public final static int SteamMystic = 11738;
+	public final static int WaterStaff = 1383;
+	public final static int WaterBattle = 1395;
+	public final static int WaterMystic = 1403;
+}
