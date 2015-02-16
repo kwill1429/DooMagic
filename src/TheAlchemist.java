ef7362894bdcb57ea54f0de3b8f598fc4f209feb
Patrick Sutton <psutton3756@yahoo.com>
2015-02-16 13:09:23 -0500

    Added very basic paint.

--- src/TheAlchemist.java
+++ src/TheAlchemist.java
@@ -6,6 +6,9 @@ import com.epicbot.api.ActiveScript;
 import com.epicbot.api.GameType;
 import com.epicbot.api.Manifest;
 import com.epicbot.api.rs3.methods.tab.Magic;
+import com.epicbot.api.rs3.methods.tab.Skills;
+import com.epicbot.api.util.SkillData;
+import com.epicbot.api.util.paint.Paint;
 import com.epicbot.event.listeners.PaintListener;
 import com.psutton.alchemist.AlchemistFrame;
 import com.psutton.alchemist.AlchemistGlobal;
@@ -18,6 +21,7 @@ public class TheAlchemist extends ActiveScript implements PaintListener
 {
 	private BankTask bankTask;
 	private CastSpellTask castSpellTask;
+	private int startingLvl;
 		
 	@Override
 	public boolean onStart() {
@@ -26,6 +30,7 @@ public class TheAlchemist extends ActiveScript implements PaintListener
 		AlchemistGlobal.timeStart = getStartTime();
 		System.out.println("Start Time: "+ AlchemistGlobal.timeStart);
 		Spells spells = new Spells();
+		startingLvl = Skills.Skill.MAGIC.getCurrentLevel();
 		
 		AlchemistGlobal.availableSpells = spells.getSpells();
 		AlchemistGlobal.spellList = spells.getSpellList();
@@ -50,7 +55,7 @@ public class TheAlchemist extends ActiveScript implements PaintListener
 //		castSpellTask = new CastSpellTask();
 //		provide(castSpellTask);
 		
-		//return true;
+//		return true;
 		return false;
 	}
 	
@@ -67,7 +72,12 @@ public class TheAlchemist extends ActiveScript implements PaintListener
 	}
 	
 	@Override
-	public void onRepaint(Graphics2D arg0) {
+	public void onRepaint(Graphics2D g) {
+		int lvlsGained = Skills.Skill.MAGIC.getCurrentLevel() - this.startingLvl;
+		String lvlString = "Magic Level: "+Skills.Skill.MAGIC.getCurrentLevel()+"("+lvlsGained+")";
+		String xpString = "XP Gained: "+SkillData.MAGIC.getXpGained();
+		Paint.drawLine(g, 0, lvlString);
+		Paint.drawLine(g, 1, xpString);
 		// TODO Auto-generated method stub
 	}
 }
