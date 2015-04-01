48ce69873aaa7262aeddf6e24df7d8d57b701025
Patrick Sutton <psutton3756@yahoo.com>
2015-04-01 12:15:47 -0400

    Added TTL stat to paint.

--- src/com/psutton/doomagic/DooMagicPaintUtil.java
+++ src/com/psutton/doomagic/DooMagicPaintUtil.java
@@ -10,25 +10,27 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 
 import com.epicbot.api.rs3.methods.tab.Skills;
+import com.epicbot.api.util.SkillData;
 import com.epicbot.api.util.Time;
 
 public class DooMagicPaintUtil 
 {
-	private int xpGained, lvlsGained;
-	private long startTime = 0;
+	private int xpGained, lvlsGained, xpPerHour;
 	private long runtime;
-	private String scriptInfoString, runtimeString, statusString, lvlString, xpString, xpPerHrString, numOfTimesCast;
 
-	private final Color frameColor = new Color(0, 0, 0, 150);
 	private final int frameTopX = 0;
 	private final int frameTopY = 200;
 	private final int frameWidth = 500;
 	private final int frameHeight = 150;
-
-	private final Color textColor = new Color(26, 73, 153, 255);
-	private final Color versionColor = new Color(255, 255, 255, 255);
 	private final int textTopX = frameTopX + 150;
 	private final int textTopY = frameTopY + 25;
+
+	private String scriptInfoString, runtimeString, statusString, lvlString, xpString;
+	private String xpPerHrString, numOfTimesCast, timeToLevelString;
+
+	private final Color frameColor = new Color(0, 0, 0, 150);
+	private final Color textColor = new Color(26, 73, 153, 255);
+	private final Color versionColor = new Color(255, 255, 255, 255);
 
 	private final Font versionFont = new Font("Helvetica Neue", Font.BOLD, 13);
 	private final Font labelFont = new Font("Helvetica Neue", Font.PLAIN, 13);
@@ -55,14 +57,16 @@ public class DooMagicPaintUtil
 
 		// Draws the first column of stats
 		g.setFont(labelFont);
-		g.drawString(statusString, textTopX, textTopY + 35);
-		g.drawString(runtimeString, textTopX, textTopY + 60);
-		g.drawString(numOfTimesCast, textTopX, textTopY + 85);
+		g.drawString(statusString, textTopX, textTopY + 25);
+		g.drawString(runtimeString, textTopX, textTopY + 50);
+		g.drawString(timeToLevelString, textTopX, textTopY + 75);
+		g.drawString(numOfTimesCast, textTopX, textTopY + 100);
+
 
 		// Draws the second column of stats
-		g.drawString(lvlString, textTopX + 215, textTopY + 35);
-		g.drawString(xpString, textTopX + 215, textTopY + 60);
-		g.drawString(xpPerHrString, textTopX + 215, textTopY + 85);
+		g.drawString(lvlString, textTopX + 215, textTopY + 25);
+		g.drawString(xpString, textTopX + 215, textTopY + 50);
+		g.drawString(xpPerHrString, textTopX + 215, textTopY + 75);
 
 		// Draws the version number
 		g.setColor(versionColor);
@@ -72,17 +76,22 @@ public class DooMagicPaintUtil
 
 	public void gatherStats()
 	{
-		lvlsGained = Skills.Skill.MAGIC.getCurrentLevel() - DooMagicGlobal.startingLvl;
-		xpGained = Skills.Skill.MAGIC.getExperience() - DooMagicGlobal.startingXP;
+		lvlsGained = SkillData.MAGIC.getLevelsGained();
+		xpGained = SkillData.MAGIC.getXpGained();
 		runtime = System.currentTimeMillis() - DooMagicGlobal.startTime;
+		xpPerHour = SkillData.MAGIC.getXpPerHour(runtime);
 
 		scriptInfoString = DooMagicGlobal.scriptName;
-		runtimeString = "Runtime: "+Time.format(runtime);
-		statusString = "Script status: "+DooMagicGlobal.scriptStatus;
-		lvlString = "Magic Level: "+Skills.Skill.MAGIC.getCurrentLevel()+"("+lvlsGained+")";
-		xpString = "XP Gained: "+xpGained;
-		xpPerHrString = "XP/Hr: "+Time.getPerHour(xpGained, startTime);
-		numOfTimesCast = "Num of Casts: "+DooMagicGlobal.numOfTimesCast;
+
+		runtimeString = "Runtime: " + Time.format(runtime);
+		statusString = "Script status: " + DooMagicGlobal.scriptStatus;
+		timeToLevelString = "TTL: " + SkillData.MAGIC.getTTL(runtime);
+		numOfTimesCast = "Num of Casts: " + DooMagicGlobal.numOfTimesCast;
+
+		lvlString = "Magic Level: "+ Skills.Skill.MAGIC.getCurrentLevel() + "(" + lvlsGained + ")";
+		xpString = "XP Gained: " + xpGained;
+		xpPerHrString = "XP/Hr: " + xpPerHour;
+
 	}
 
 	public Image getScriptImage() {		
