4a462c2dc935560f8c2f0874d18456fd0bbd2ac8
Patrick Sutton <psutton3756@yahoo.com>
2015-04-03 17:51:03 -0400

    Fixed issues with calculating points based on screensize.

--- src/com/psutton/utilities/helpers/MagicHelper.java
+++ src/com/psutton/utilities/helpers/MagicHelper.java
@@ -1,41 +1,78 @@
 package com.psutton.utilities.helpers;
 
 import com.epicbot.api.rs3.methods.Calculations;
+import com.epicbot.api.rs3.methods.Game;
 
 public class MagicHelper {
-    private Calculations.OnScreenManager onScreenManager;
 
-    public static int getAdjustedXCoord(int x) {
-        int offset;
-
-        if (isBigScreen() == true) {
-            offset = 200;
-        } else {
-            offset = 219;
-        }
-
-        return (x - offset);
-    }
-
-    public static int getAdjustedYCoord(int y) {
-        int offset;
+    public static int centerX(int spellBookPosition) {
+        int increment = 40; // Width of spell + horizontal spacer
+        int baseX, leftX, rightX, centerX;
 
         if (isBigScreen()) {
-            offset = 332;
+            baseX = 600 + (Game.getWidth() - 800);
         } else {
-            offset = 309;
+            baseX = 546 + (Game.getWidth() - 765);
         }
 
-        return (y - offset);
+        if ((spellBookPosition + 4) % 5 == 0) {
+            leftX = baseX;
+        } else if ((spellBookPosition + 3) % 5 == 0) {
+            leftX = baseX + increment;
+        } else if ((spellBookPosition + 2) % 5 == 0) {
+            leftX = baseX + (2 * increment);
+        } else if ((spellBookPosition + 1) % 5 == 0) {
+            leftX = baseX + (3 * increment);
+        } else {
+            leftX = baseX + (4 * increment);
+        }
+
+        rightX = leftX + 29;
+        centerX = (leftX + rightX) / 2;
+
+        return (centerX);
     }
 
-    private static boolean isBigScreen() {
-        Calculations.OnScreenManager onScreenManager = Calculations.OnScreenManager.instance();
+    public static int centerY(int spellBookPosition) {
+        int increment = 34; // Height of spell + vertical spacer
+        int topY = 0;
+        int baseY, bottomY, centerY;
 
-        if (onScreenManager.onCanvas(801, 100) == false) {
-            return false;
+        if (isBigScreen()) {
+            baseY = 208 + (Game.getHeight() - 540);
+        } else {
+            baseY = 249 + (Game.getHeight() - 540);
         }
 
-        return true;
+        if (spellBookPosition < 6) {
+            topY = baseY;
+        } else if (spellBookPosition < 11) {
+            topY = baseY + increment;
+        } else if (spellBookPosition < 16) {
+            topY = baseY + (2 * increment);
+        } else if (spellBookPosition < 21) {
+            topY = baseY + (3 * increment);
+        } else if (spellBookPosition < 26) {
+            topY = baseY + (4 * increment);
+        } else if (spellBookPosition < 31) {
+            topY = baseY + (5 * increment);
+        } else if (spellBookPosition < 36) {
+            topY = baseY + (6 * increment);
+        }
+
+        bottomY = topY + 29;
+        centerY = (topY + bottomY) / 2;
+
+        return (centerY);
+    }
+
+    public static boolean isBigScreen() {
+        Calculations.OnScreenManager onScreenManager = Calculations.OnScreenManager.instance();
+
+        if (onScreenManager.onCanvas(800, 100)) {
+            return true;
+        }
+
+        return false;
     }
 }
