7d027c8785a29b9d4828c44e8ba65db47e6dcc95
Patrick Sutton <psutton3756@yahoo.com>
2015-04-03 18:18:09 -0400

    Added calls to setSpellPosition property that was added to PSSpells.

--- src/com/psutton/doomagic/Spells.java
+++ src/com/psutton/doomagic/Spells.java
@@ -1,14 +1,13 @@
 package com.psutton.doomagic;
 
-import java.awt.Point;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Set;
-
 import com.epicbot.api.rs3.methods.tab.Magic;
 import com.psutton.utilities.items.RuneList;
 import com.psutton.utilities.objects.PSRune;
 import com.psutton.utilities.objects.PSSpell;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Spells {
 	private PSSpell lowAlch, highAlch;
@@ -34,10 +33,10 @@ public class Spells {
 		PSRune[] lowAlchRunes = { lowAlchOne, lowAlchTwo };
 		lowAlch = new PSSpell("Low Level Alchemy", Magic.Spell.LOW_LEVEL_ALCHEMY);
 		lowAlch.setTimeToCast(3000);
-		lowAlch.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
+		lowAlch.setAbilityTab(Magic.InnerAbilityTab.SKILLING_SPELL);
 		lowAlch.setRequiresAnItem(true);
 		lowAlch.setRunes(lowAlchRunes);
-		lowAlch.setCentralCoordinate(new Point(715, 323));
+		lowAlch.setSpellPosition(4);
 
 		PSRune highAlchOne = new PSRune(natureRune, 1);
 		PSRune highAlchTwo = new PSRune(fireRune, 1);
@@ -47,7 +46,7 @@ public class Spells {
 		highAlch.setAbilityTab(Magic.InnerAbilityTab.SKILLING_SPELL);
 		highAlch.setRequiresAnItem(true);
 		highAlch.setRunes(highAlchRunes);
-		highAlch.setCentralCoordinate(new Point(715, 357));
+		highAlch.setSpellPosition(9);
 
 		//		PSRune superheatOne = new PSRune(natureRune, 1);
 		//		PSRune superheatTwo = new PSRune(fireRune, 4);
@@ -65,9 +64,9 @@ public class Spells {
 		PSRune[] mobilisingArmiesRunes = { mobilisingOne, mobilisingTwo, mobilisingThree };
 		mobilisingArmiesTeleport = new PSSpell("Mobilising Armies Teleport", Magic.Spell.MOBILISING_ARMIES_TELEPORT);
 		mobilisingArmiesTeleport.setTimeToCast(3000);
-		mobilisingArmiesTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		mobilisingArmiesTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		mobilisingArmiesTeleport.setRunes(mobilisingArmiesRunes);
-		mobilisingArmiesTeleport.setCentralCoordinate(new Point(635, 323));
+		mobilisingArmiesTeleport.setSpellPosition(2);
 
 		PSRune varrockOne = new PSRune(lawRune, 1);
 		PSRune varrockTwo = new PSRune(airRune, 3);
@@ -75,9 +74,9 @@ public class Spells {
 		PSRune[] varrockTeleRunes = { varrockOne, varrockTwo, varrockThree };
 		varrockTeleport = new PSSpell("Varrock Teleport", Magic.Spell.VARROCK_TELEPORT);
 		varrockTeleport.setTimeToCast(3500);
-		varrockTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		varrockTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		varrockTeleport.setRunes(varrockTeleRunes);
-		varrockTeleport.setCentralCoordinate(new Point(675, 323));
+		varrockTeleport.setSpellPosition(3);
 
 		PSRune lumbridgeOne = new PSRune(lawRune, 1);
 		PSRune lumbridgeTwo = new PSRune(airRune, 3);
@@ -85,9 +84,9 @@ public class Spells {
 		PSRune[] lumbridgeTeleRunes = { lumbridgeOne, lumbridgeTwo, lumbridgeThree };
 		lumbridgeTeleport = new PSSpell("Lumbridge Teleport", Magic.Spell.LUMBRIDGE_TELEPORT);
 		lumbridgeTeleport.setTimeToCast(3500);
-		lumbridgeTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		lumbridgeTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		lumbridgeTeleport.setRunes(lumbridgeTeleRunes);
-		lumbridgeTeleport.setCentralCoordinate(new Point(715, 323));
+		lumbridgeTeleport.setSpellPosition(4);
 
 		PSRune faladorOne = new PSRune(lawRune, 1);
 		PSRune faladorTwo = new PSRune(airRune, 3);
@@ -95,9 +94,9 @@ public class Spells {
 		PSRune[] faladorTeleRunes = { faladorOne, faladorTwo, faladorThree };
 		faladorTeleport = new PSSpell("Falador Teleport", Magic.Spell.FALADOR_TELEPORT);
 		faladorTeleport.setTimeToCast(3500);
-		faladorTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		faladorTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		faladorTeleport.setRunes(faladorTeleRunes);
-		faladorTeleport.setCentralCoordinate(new Point(755, 323));
+		faladorTeleport.setSpellPosition(5);
 
 		PSRune houseOne = new PSRune(lawRune, 1);
 		PSRune houseTwo = new PSRune(airRune, 1);
@@ -105,36 +104,36 @@ public class Spells {
 		PSRune[] houseTeleRunes = { houseOne, houseTwo, houseThree };
 		houseTeleport = new PSSpell("House Teleport", Magic.Spell.HOME_TELEPORT);
 		houseTeleport.setTimeToCast(3500);
-		houseTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		houseTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		houseTeleport.setRunes(houseTeleRunes);
-		houseTeleport.setCentralCoordinate(new Point(595, 357));
+		houseTeleport.setSpellPosition(6);
 
 		PSRune camelotOne = new PSRune(lawRune, 1);
 		PSRune camelotTwo = new PSRune(airRune, 5);
 		PSRune[] camelotTeleRunes = { camelotOne, camelotTwo };
 		camelotTeleport = new PSSpell("Camelot Teleport", Magic.Spell.CAMELOT_TELEPORT);
 		camelotTeleport.setTimeToCast(3500);
-		camelotTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		camelotTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		camelotTeleport.setRunes(camelotTeleRunes);
-		camelotTeleport.setCentralCoordinate(new Point(635, 357));
+		camelotTeleport.setSpellPosition(7);
 
 		PSRune ardougneOne = new PSRune(lawRune, 2);
 		PSRune ardougneTwo = new PSRune(waterRune, 2);
 		PSRune[] ardougneTeleRunes = { ardougneOne, ardougneTwo };
 		ardougneTeleport = new PSSpell("Ardougne Teleport", Magic.Spell.ARDOUGNE_TELEPORT);
 		ardougneTeleport.setTimeToCast(3500);
-		ardougneTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		ardougneTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		ardougneTeleport.setRunes(ardougneTeleRunes);
-		ardougneTeleport.setCentralCoordinate(new Point(675, 357));
+		ardougneTeleport.setSpellPosition(8);
 
 		PSRune trollheimOne = new PSRune(lawRune, 2);
 		PSRune trollheimTwo = new PSRune(fireRune, 2);
 		PSRune[] trollheimTeleRunes = { trollheimOne, trollheimTwo };
 		trollheimTeleport = new PSSpell("Trollheim Teleport", Magic.Spell.TROLLHEIM_TELEPORT);
 		trollheimTeleport.setTimeToCast(3500);
-		trollheimTeleport.setAbilityTab(Magic.InnerAbilityTab.COMBAT_SPELL);
+		trollheimTeleport.setAbilityTab(Magic.InnerAbilityTab.TELEPORT_SPELL);
 		trollheimTeleport.setRunes(trollheimTeleRunes);
-		//		trollheimTeleport.setCentralCoordinate(new Point(675, 357));
+		trollheimTeleport.setSpellPosition(11);
 
 		generateSpells();
 	}
